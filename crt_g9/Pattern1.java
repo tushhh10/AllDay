976c31f836e366e6b89825594d9eee10c7b1ba45
Your Name <22bk1a05g9@stpetershyd.com>
2025-01-24 14:55:40 +0530

    ck

--- crt_g9/Pattern1.java
+++ crt_g9/Pattern1.java
@@ -5,7 +5,7 @@ public class Pattern1 {
 	public static void main(String[] args) {
 		for(int r=5;r>=1;r--) {
 			for(int c=5;c>=r;c--) {
-				System.out.print(r+" ");
+				System.out.print(c+" ");
 			}
 			System.out.println();
 		}
---
+++ crt_g9/Pattern2.java
@@ -0,0 +1,16 @@
+package crt_g9;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+			for(int r=1;r<=5;r++) {
+				for(int c=5;c>=r;c--) {
+					System.out.print(c+" ");
+				}
+				System.out.println();
+			}
+
+		}
+}
+
+

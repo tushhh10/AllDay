4b01da5fe1079957d936a5f4d8046e5acb784479
Your Name <22bk1a05g9@stpetershyd.com>
2025-01-24 10:51:15 +0530

    3 commit

--- crt_g9/PrimeNov2.java
+++ crt_g9/PrimeNov2.java
@@ -4,10 +4,18 @@ public class PrimeNov2 {
 
 	public static void main(String[] args) {
 		for(int i=1,k=0;i<100;i++) {
-			int num=1;
+			int num=i;
 			int counter=0;
 			for(int p=1;p<=num;p++) {
-				if(num%p==0)
+				if(num%p==0) counter++;
+			}
+			if(counter == 2) {
+				System.out.print(num+" ");
+				k++;
+				if(k==6) {
+					System.out.println();
+					k=0;
+				}
 			}
 		}
 

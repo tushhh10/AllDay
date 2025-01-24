e43a7bf298d8de4098aebc5ccbbe75cc054497dc
Your Name <22bk1a05g9@stpetershyd.com>
2025-01-24 10:34:30 +0530

    2 commit

--- crt_g9/PrimeNov.java
+++ crt_g9/PrimeNov.java
@@ -3,8 +3,12 @@ package crt_g9;
 public class PrimeNov {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int num=31,counter=0;
+		for(int p=num;p>=1;p--) {
+			if(num%p==0) counter++;
+		}
+		if(counter == 2) System.out.println("yes it is prime");
+		else System.out.println("not a prime");	
 	}
-
 }
---
+++ crt_g9/PrimeNov2.java
@@ -0,0 +1,16 @@
+package crt_g9;
+
+public class PrimeNov2 {
+
+	public static void main(String[] args) {
+		for(int i=1,k=0;i<100;i++) {
+			int num=1;
+			int counter=0;
+			for(int p=1;p<=num;p++) {
+				if(num%p==0)
+			}
+		}
+
+	}
+
+}

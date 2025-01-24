a367a72ef4d1514137a95b91be273d80928b173d
Your Name <22bk1a05g9@stpetershyd.com>
2025-01-24 15:26:49 +0530

    com

---
+++ crt_g9/Pattern4.java
@@ -0,0 +1,27 @@
+package crt_g9;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		for(int r=1;r<=15;r++) {
+			for(int k=1;k<17;k++) {
+				System.out.print(" ");
+			}
+			for(int c=1;c<=30;c++) {
+			if((r==1) || (r==15)) {
+				System.out.print("*");
+			}
+			else {
+				if((c==1) || (c==30)) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+		}
+		System.out.println();
+	}
+	}
+}
+
